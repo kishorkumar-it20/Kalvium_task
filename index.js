1802f9c307a5622f6dcab47e3899574ca333c6ee
KISHORKUMAR D <92205078+kishorkumar-it20@users.noreply.github.com>
2023-04-25 18:58:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const express = require("express");
+const cors = require("cors");
+const Axios = require("axios");
+const app = express();
+const PORT = 8080;
+ 
+app.use(cors());
+app.use(express.json());
+ 
+app.post("/compile", (req, res) => {
+    //getting the required data from the request
+    let code = req.body.code;
+    let language = req.body.language;
+    let input = req.body.input;
+ 
+    if (language === "python") {
+        language="py"
+    }
+ 
+    let data = {
+        "code": code,
+        "language": language,
+        "input": input
+    };
+    let config = {
+        method: 'post',
+        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    console.log(config);
+    // calling the code compilation API
+    Axios(config)
+        .then((response)=>{
+            res.send(response.data)
+            console.log("Response from server : ", response.data)
+        }).catch((error)=>{
+            console.log("Error occured : ",error);
+        });
+})
+ 
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
